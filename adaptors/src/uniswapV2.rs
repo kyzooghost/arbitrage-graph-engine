cfb7535872e29c55800edb48dfa4d5dd30dbba93
kyzooghost <jeffersonzjtang@gmail.com>
2022-11-12 06:20:53 +1100

    created uniswap unit tests

--- adaptors/src/uniswapV2.rs
+++ adaptors/src/uniswapV2.rs
@@ -2,38 +2,37 @@
 
 pub use uniswapV2_mod::*;
 
-use ethers::{
-    providers::{Provider, JsonRpcClient},
-    contract::Contract,
-    types::Address,
-    core::abi::Abi
-};
-
-use std::{
-    fs::File,
-    sync::Arc,
-};
-
-use eyre::Result;
-use arbitrage_engine::utils::logger::{logText, logObject};
-use futures::future::join_all;
-use super::utils::{is_zero_address, are_addresses_equal};
-
 mod uniswapV2_mod {
-    use super::{
-        File,
-        Arc,
-        Abi,
-        Address,
-        Provider,
-        Contract,
-        JsonRpcClient,
-        Result,
-        logText,
-        logObject,
-        join_all,
-        is_zero_address,
-        are_addresses_equal
+    use ethers::{
+        providers::{Provider, JsonRpcClient},
+        contract::Contract,
+        types::{Address, U64, U128, U256},
+        core::abi::Abi
+    };
+    
+    use std::{
+        fs::File,
+        sync::Arc,
+        collections::HashMap,
+        time::{Duration, Instant}
+    };
+    
+    use eyre::Result;
+    use arbitrage_engine::{
+        utils::logger::{logText, logObject},
+        graph::cycle::get_negative_cycle_quick
+    };
+    use futures::future::join_all;
+    use super::super::utils::{
+        is_zero_address, 
+        are_addresses_equal,
+        one_ether,
+        u256_inverse,
+        u256_to_f64,
+    };
+    use petgraph::{
+        graph::Graph,
+        prelude::{EdgeIndex, NodeIndex},
     };
 
     #[derive(Debug)]
@@ -41,6 +40,14 @@ mod uniswapV2_mod {
         token0: Address,
         token1: Address,
         lptoken: Address
+    }
+
+    #[derive(Debug)]
+    struct TokenExchangeRates {
+        token0: Address,
+        token1: Address,
+        token0_to_token1_rate: f64,
+        token1_to_token0_rate: f64,
     }
 
     #[derive(Debug)]
@@ -90,11 +97,19 @@ mod uniswapV2_mod {
                 if wrapped_pairs[i].is_ok() && wrapped_pairs[i].as_ref().unwrap().as_ref().is_ok() {
                     let pair = wrapped_pairs[i].as_ref().unwrap().as_ref().unwrap();
                     if !is_zero_address(*pair) {
-                        self.token_pairs.push(TokenPair{
-                            token0: *token,
-                            token1: token_address,
-                            lptoken: *pair
-                        });
+                        if *token > token_address {
+                            self.token_pairs.push(TokenPair{
+                                token0: token_address,
+                                token1: *token,
+                                lptoken: *pair
+                            });
+                        } else {
+                            self.token_pairs.push(TokenPair{
+                                token0: *token,
+                                token1: token_address,
+                                lptoken: *pair
+                            });
+                        }
                     }
                 }
             }
@@ -137,6 +152,75 @@ mod uniswapV2_mod {
         pub fn token_pairs(&self) -> &Vec<TokenPair> {
             &self.token_pairs
         }
+
+        pub async fn find_local_arbitrage(&self) -> Result<()> {
+            // Build graph with all tokens as nodes
+            let mut graph: Graph<Address, f64> = Graph::new();
+            let mut nodes: HashMap<Address, NodeIndex> = HashMap::new();
+            for i in 0..self.tokens.len() {
+                nodes.insert(self.tokens[i], graph.add_node(self.tokens[i]));
+            }
+
+            // Get rates for each pair
+            let pair_abi_file = File::open("src/abis/UniswapV2Pair.json")?;
+            let pair_abi: Abi = serde_json::from_reader(pair_abi_file)?;
+            let mut get_reserve_futures = Vec::new();
+
+            for pair_address in self.token_pairs.iter() {
+                let pair_contract: Contract<Arc<Provider<P>>> = Contract::new(pair_address.lptoken, pair_abi.clone(), Arc::clone(&self.provider));
+
+                let get_reserve_future = pair_contract
+                    .method::<_, (U256, U256, U256)>("getReserves", ())?
+                    .call()
+                    .await;
+
+                get_reserve_futures.push(tokio::spawn(async move {get_reserve_future}));
+            }
+            let wrapped_reserves = join_all(get_reserve_futures).await;
+            let mut pairs_with_price: Vec<TokenExchangeRates> = Vec::new();
+            for (i, pair_struct) in self.token_pairs.iter().enumerate() {
+                if wrapped_reserves[i].is_ok() && wrapped_reserves[i].as_ref().unwrap().as_ref().is_ok() {
+                    let reserve = wrapped_reserves[i].as_ref().unwrap().as_ref().unwrap();
+                    let reserve0 = reserve.0;
+                    let reserve1 = reserve.1;
+
+                    // Use UniswapV2Router.getAmountOut() implementation, with 1e18 token0 going in
+                    let amount_in_with_fee = one_ether().checked_mul(U256::from_dec_str("997")?).unwrap();
+                    let numerator = amount_in_with_fee.checked_mul(reserve1).unwrap();
+                    let denominator = reserve0.checked_mul(U256::from_dec_str("1000")?).unwrap().checked_add(amount_in_with_fee).unwrap();
+                    let token0_to_token1_rate = numerator.checked_div(denominator).unwrap();
+                    let token1_to_token0_price = u256_inverse(token0_to_token1_rate);
+
+                    pairs_with_price.push(TokenExchangeRates {
+                        token0: pair_struct.token0,
+                        token1: pair_struct.token1,
+                        token0_to_token1_rate: u256_to_f64(token0_to_token1_rate),
+                        token1_to_token0_rate: u256_to_f64(token1_to_token0_price),
+                    });
+                }
+            }
+
+            // Add rates as nodes to graph
+            for pair in pairs_with_price.iter() {
+                graph.add_edge(
+                    *nodes.get(&pair.token0).unwrap(), 
+                    *nodes.get(&pair.token1).unwrap(),
+                    pair.token0_to_token1_rate
+                );
+
+                graph.add_edge(
+                    *nodes.get(&pair.token1).unwrap(), 
+                    *nodes.get(&pair.token0).unwrap(),
+                    pair.token1_to_token0_rate
+                );
+            }
+
+            let (is_arbitrage, arbitrage_path) = get_negative_cycle_quick(&graph);
+
+            logObject("is_arbitrage: ", &is_arbitrage);
+            logObject("arbitrage_path: ", &arbitrage_path);
+            Ok(())
+        }
     }
 
     #[tokio::test]
@@ -169,9 +253,76 @@ mod uniswapV2_mod {
         arbitrage_finder.add_token(bal).await?;
         assert!(arbitrage_finder.tokens().len() == 3_usize);
         assert!(arbitrage_finder.token_pairs().len() == 3_usize);
+        arbitrage_finder.find_local_arbitrage().await?;
 
+        // let rad = "0x31c8eacbffdd875c74b94b077895bd78cf1e64a3".parse::<Address>()?;
+        // let paxg = "0x45804880de22913dafe09f4980848ece6ecbaf78".parse::<Address>()?;
+        // let uni = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984".parse::<Address>()?;
+        // let elon = "0x761d38e5ddf6ccf6cf7c55759d5210750b5d60f3".parse::<Address>()?;
+        // let fox = "0xc770eefad204b5180df6a14ee197d99d808ee52d".parse::<Address>()?;
+        // let wbtc = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599".parse::<Address>()?;
+        // arbitrage_finder.add_token(rad).await?;
+        // arbitrage_finder.add_token(paxg).await?;
+        // arbitrage_finder.add_token(uni).await?;
+        // arbitrage_finder.add_token(elon).await?;
+        // arbitrage_finder.add_token(fox).await?;
+        // arbitrage_finder.add_token(wbtc).await?;
+        // let start = Instant::now();
+        // arbitrage_finder.find_local_arbitrage().await?;
+        // let duration = start.elapsed();
+        // println!("Time elapsed in find_local_arbitrage() is: {:?}", duration);
         Ok(())
     }
-    // getCurrentPrice
-    // Query all price pairs for given tokens, create graph, process in arbitrage engine
+
+    #[tokio::test]
+    async fn spookyswap_basic_integration_test() -> Result<()> {
+        let provider = Provider::try_from("https://rpc.ftm.tools")?;
+        let factory_address = "0x152ee697f2e276fa89e96742e9bb9ab1f2e61be3".parse::<Address>()?;
+        let router_address = "0xf491e7b69e4244ad4002bc14e878a34207e38c29".parse::<Address>()?;
+        let mut arbitrage_finder = UniswapV2ArbitrageFinder::new(factory_address, router_address, provider)?;
+        assert!(&arbitrage_finder.tokens().is_empty());
+
+        let wftm = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83".parse::<Address>()?;
+        let usdc = "0x04068da6c83afcfa0e13ba15a6696662335d5b75".parse::<Address>()?;
+        let spa = "0x5602df4a94eb6c680190accfa2a475621e0ddbdc".parse::<Address>()?;
+        let eth = "0x74b23882a30290451a17c44f4f05243b6b58c76d".parse::<Address>()?;
+        let btc = "0x321162cd933e2be498cd2267a90534a804051b11".parse::<Address>()?;
+        let boo = "0x841fad6eae12c286d1fd18d1d525dffa75c7effe".parse::<Address>()?;
+        let midas = "0xb37528da6b4d378305d000a66ad91bd88e626761".parse::<Address>()?;
+        let hec = "0x5c4fdfc5233f935f20d2adba572f770c2e377ab0".parse::<Address>()?;
+        let link = "0xb3654dc3d10ea7645f8319668e8f54d2574fbdc8".parse::<Address>()?;
+        let mimatic = "0xfb98b335551a418cd0737375a2ea0ded62ea213b".parse::<Address>()?;
+        let tomb = "0x6c021ae822bea943b2e66552bde1d2696a53fbb7".parse::<Address>()?;
+        let tor = "0x74e23df9110aa9ea0b6ff2faee01e740ca1c642e".parse::<Address>()?;
+        let mim = "0x82f0b8b456c1a451378467398982d4834b6829c1".parse::<Address>()?;
+        let stg = "0x2f6f07cdcf3588944bf4c42ac74ff24bf56e7590".parse::<Address>()?;
+        let bnb = "0xd67de0e0a0fd7b15dc8348bb9be742f3c5850454".parse::<Address>()?;
+        let geist = "0xd8321aa83fb0a4ecd6348d4577431310a6e0814d".parse::<Address>()?;
+        let scream = "0xe0654c8e6fd4d733349ac7e09f6f23da256bf475".parse::<Address>()?;
+        let oath = "0x21ada0d2ac28c3a5fa3cd2ee30882da8812279b6".parse::<Address>()?;
+
+        let start = Instant::now();
+        arbitrage_finder.add_token(wftm).await?;
+        arbitrage_finder.add_token(usdc).await?;
+        arbitrage_finder.add_token(spa).await?;
+        arbitrage_finder.add_token(eth).await?;
+        arbitrage_finder.add_token(btc).await?;
+        arbitrage_finder.add_token(boo).await?;
+        arbitrage_finder.add_token(midas).await?;
+        arbitrage_finder.add_token(hec).await?;
+        arbitrage_finder.add_token(link).await?;
+        arbitrage_finder.add_token(mimatic).await?;
+        arbitrage_finder.add_token(tomb).await?;
+        arbitrage_finder.add_token(tor).await?;
+        arbitrage_finder.add_token(mim).await?;
+        arbitrage_finder.add_token(stg).await?;
+        arbitrage_finder.add_token(bnb).await?;
+        arbitrage_finder.add_token(geist).await?;
+        arbitrage_finder.add_token(scream).await?;
+        arbitrage_finder.add_token(oath).await?;
+        arbitrage_finder.find_local_arbitrage().await?;
+        let duration = start.elapsed();
+        println!("Time elapsed in find_local_arbitrage() is: {:?}", duration);
+        Ok(())
+    }
 }
--- adaptors/src/utils.rs
+++ adaptors/src/utils.rs
@@ -1,7 +1,9 @@
 pub use utils_mod::*;
 
 mod utils_mod {
-    use ethers::types::Address;
+    use std::ops::Div;
+
+    use ethers::types::{Address, U256};
     
     pub fn is_zero_address(address: Address) -> bool {
         address.to_fixed_bytes() == "0x0000000000000000000000000000000000000000".parse::<Address>().unwrap().to_fixed_bytes()
@@ -9,5 +11,23 @@ mod utils_mod {
 
     pub fn are_addresses_equal(address1: Address, address2: Address) -> bool {
         address1.to_fixed_bytes() == address2.to_fixed_bytes()
+    }
+
+    pub fn one_ether() -> U256 {
+        U256::exp10(18)
+    }
+
+    pub fn u256_inverse(x: U256) -> U256 {
+        U256::exp10(36).checked_div(x).unwrap()
+    }
+
+    pub fn u256_to_f64(x: U256) -> f64 {
+        let (integer, mantissa) = x.div_mod(one_ether());
+        // So this will error if our integer larger than 1e18?
+        let integer_as_f64 = integer.as_u64() as f64;
+        // Mantissa is % 1e18, so cannot be bigger than 1e18 by definition
+        // u64 as safely handle numbers up to ~1e19
+        let mantissa_as_f64 = mantissa.as_u64() as f64 / 10_u64.pow(18) as f64;
+        integer_as_f64 + mantissa_as_f64
     }
 }
